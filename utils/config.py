a61168c7f6276bb10b1679e1af8ec93e9667a7a4
TamaraFHGR <162818989+TamaraFHGR@users.noreply.github.com>
2025-12-16 06:58:48 +0100

    API key revocation due to publication of repo

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,8 @@
 # config.py
 
 # ----- Groq API Key -----
+"""
+The API key has been revoked due to the publication of the Git repository. In case you wish to run the scripts ´data_pipeline/i_2_extract_additional_villains_with_LLM.py´ or
+´data_pipeline/i_4_enrich_villains_data_with_LLM.py´ a new key needs to be generated.
+"""
 GROQ_API_KEY = "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"
